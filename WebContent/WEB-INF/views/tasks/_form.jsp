df08ed4f81c28a725fa6b85cc7650c90b8cab267
wid_ds_training <wid_ds_training@QAW400>
2022-05-20 16:55:46 +0900

    「バリデーションを追加」

--- WebContent/WEB-INF/views/tasks/_form.jsp
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -1,4 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%-- エラーフラッシュ --%>
+<c:if test="${errors != null}">
+	<div id="flush_error">
+		入力エラーがあります！<br/>
+
+		<c:forEach var="error" items="${errors}">
+			・<c:out value="${error}"/><br/>
+		</c:forEach>
+	</div>
+</c:if>
+
 <%-- Newのリクエストスコープに入れている --%>
 <label for="content">新しい項目</label><br/>
 <input type="text" name="content" value="${ task.content }"/>
---
+++ src/models/validators/TaskValidator.java
@@ -0,0 +1,31 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class TaskValidator {
+	//バリデーションの実行(エラーが出たら入れていく)
+    public static List<String> validate(Task t) {
+        List<String> errors = new ArrayList<String>();
+
+
+        String content_error = validateContent(t.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+
+    // メッセージの必須入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "項目を入力してください。";
+        }
+
+        return "";
+    }
+}
