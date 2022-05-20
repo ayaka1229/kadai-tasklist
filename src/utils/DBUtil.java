bef095e9793efee29578edf7d101fe2195f3690b
wid_ds_training <wid_ds_training@QAW400>
2022-05-20 09:46:55 +0900

    「データベース関連のファイルを追加」

---
+++ src/utils/DBUtil.java
@@ -0,0 +1,26 @@
+package utils;
+
+//DAO
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DBUtil {
+	private static final String PERSISTANCE_UNIT_NAME = "tasklist";
+	private static EntityManagerFactory emf;
+
+	public static EntityManager createEntityMnager() {
+		return getEntityManagerFactory().createEntityManager();
+	}
+
+	private static EntityManagerFactory getEntityManagerFactory() {
+		if(emf == null)
+		{
+			emf = Persistence.createEntityManagerFactory(PERSISTANCE_UNIT_NAME);
+		}
+
+		return emf;
+	}
+
+}
